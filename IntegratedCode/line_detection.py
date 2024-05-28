18c14de0551a70d97ff9cc149706478e12431a4c
bsafaeian <155590184+bsafaeian@users.noreply.github.com>
2024-05-28 15:56:20 -0700

    Update line_detection.py
    Added improved documentation.

--- IntegratedCode/line_detection.py
+++ IntegratedCode/line_detection.py
@@ -1,5 +1,5 @@
 """
-@brief This program demonstrates line finding with the Hough transform
+@brief This program demonstrates line finding with the Hough and Homography transforms
 """
 import sys
 import math
@@ -11,6 +11,7 @@ from line_detect_connected import remove_outliers
 import track_ball
 import os
 
+#the main function runs both the manual and automated tracking
 def main(argv):
     default_file = r'5GProject/videos/My_Movie_6.mov' # example file to replace
     filename = argv[0] if len(argv) > 0 else default_file
@@ -31,14 +32,15 @@ def main(argv):
 
         fourcc = cv.VideoWriter_fourcc(*'mp4v')  # Codec for MP4
 
-
+        #create names for the processed videos
         base_name, _ = os.path.splitext(filename)
-        automated_lines_path = os.path.basename(base_name+"_auto_lines") + ".mp4"
-        manual_lines_path =  os.path.basename(base_name+"_manual_lines") + ".mp4"
+        automated_lines_path = os.path.basename(base_name+"_auto_lines") + ".mp4" #automated tracking gets tagged with _auto_lines
+        manual_lines_path =  os.path.basename(base_name+"_manual_lines") + ".mp4" #manual tracking gets tagged with _manual_lines
 
         automated_lines_path = os.path.join('videos', automated_lines_path)
         manual_lines_path = os.path.join('videos', manual_lines_path)
         
+        #video is saved in an mp4 format in the same directory as the source video
         automated_lines = cv.VideoWriter(automated_lines_path, fourcc, fps, (frame_width, frame_height))
         manual_lines = cv.VideoWriter(manual_lines_path, fourcc, fps, (frame_width, frame_height))
 
@@ -114,12 +116,15 @@ def main(argv):
                 cv.line(cdstP, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 3, cv.LINE_AA)
 
         if is_video:
+
+            #write the result of the manual tracking to the repective video files
             manual_lines.write(manual_track_image)
             automated_lines.write(cdstP)
 
     return 0
 
 
+#operates in the same way as main, but the resulting images are exportable directly using a function call
 def lineTracking(src,birdseye_field_path,manual_track_image= None):
     '''
     Parameters:
@@ -181,7 +186,21 @@ def lineTracking(src,birdseye_field_path,manual_track_image= None):
     return cdstP,manual_track_image
 
 
+#only performs the manual tracking on a given video
 def manual_Tracking(path,output_videos_path):
+    '''
+    Brief:
+    Takes in a video path. Performs manual tracking using a Homography transform to warp the map of the field. The output video has the clairified lines on the field. 
+    The location of the output video is specified by output_videos_path.
+
+    Parameters:
+    path: path to the raw video/stream .
+    output_videos_path: output directory of the manually tracked videos.
+
+    Returns:
+    errorCode: returns 0 if successful, returns -1 if an error occured.
+
+    '''
     filename = path
     # Determine if the input is an image or video
     is_video = filename.endswith(('.mp4', '.avi', '.mkv','.mov','.MOV')) # for video choices
@@ -243,22 +262,26 @@ def manual_Tracking(path,output_videos_path):
         if is_video:
             manual_lines.write(combined_image)
 
-
     return 0
 
+
+'''
+The user runs the program like so:
+python3 line_detection <videos folder> <line detection output directory> <ball tracking output directory>.
+'''
 if __name__ == "__main__":
-    videos = os.listdir(sys.argv[1])
+    videos = os.listdir(sys.argv[1]) #take in videos folder as the 1st argument
     paths = []
     video_names = []
     
     for video in videos:
         
         video_names.append(video)
-        path = sys.argv[2] + "/" + video
+        path = sys.argv[2] + "/" + video #take in the output folder as the 2nd argument
         paths.append(path)
-        manual_Tracking(sys.argv[1] + "/" + video, sys.argv[2])
+        manual_Tracking(sys.argv[1] + "/" + video, sys.argv[2]) # videos will be saved as <line detection folder>/videoname. The video name remains the same.
 
     print(paths)
-    output_folder = sys.argv[3]
-    track_ball.process_videos([paths,output_folder])
-    track_ball.save_output(output_folder,video_names)
+    output_folder = sys.argv[3] #take in output directory for ball tracking
+    track_ball.process_videos([paths,output_folder]) # after line tracking is concluded, the output is pipelined into the ball tracking algorithm
+    track_ball.save_output(output_folder,video_names) # output of the videos are saved.
